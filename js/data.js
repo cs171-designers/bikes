bb920ba2140dc79df2386e08d402c4ec26b9f611
protograph-io <protograph.io@gmail.com>
2021-11-11 23:50:00 -0500

    Updated data handler with min files

--- js/data.js
+++ js/data.js
@@ -2,6 +2,9 @@
 
 // Has two very useful properties but should not be exposed: _rides and _stations
 class DataHandler {
+    station_files = [
+        "stations.csv",
+    ]
     files = [
         // // modified to get rid of extra header row
         "2018/201801_hubway_tripdata.csv",
@@ -33,25 +36,43 @@ class DataHandler {
         console.log("Data constructor");
     }
     load() {
+        return this.loadStations().then(() => {
+            return this.loadRides();
+        })
+    }
+    loadStations() {
+        let USE_MIN = true; // must be true
         let dataHandler = this;
-        let USE_MIN = true;
-        console.log("begin loading", "with min", USE_MIN)
-        Promise.all([...this.files.map(f => d3.csv("data/" + (USE_MIN ? "min/" : "") + f, d3.autoType))])
-            .then(function ([stations, old_stations, ...data]) {
-                //console.log(data.length)
-                //console.log("done", stations, old_stations, data)
-
-                // process stations data
-                dataHandler._stations = [...stations, ...old_stations].map(item => {
+        console.log("loading stations")
+        return Promise.all([...this.station_files.map(f => d3.csv("data/" + (USE_MIN ? "min/" : "") + f, d3.autoType))])
+            .then(([stations]) => {
+                dataHandler._stations = stations.map(item => {
                     if ("Public" in item) {
                         item.Public = (item.Public === "Yes");
                     }
                     return item;
-                }).filter((item, index, array) => {
-                    // filter out duplicate stations
-                    return index === array.findIndex(other => item.Number === other.Number);
-                });
-
+                })
+                // .filter((item, index, array) => {
+                //     // filter out duplicate stations
+                //     return index === array.findIndex(other => item.Number === other.Number);
+                // });
+                console.log("stations", dataHandler._stations);
+                console.log("one station");
+                console.table(dataHandler._stations[0]);
+                return dataHandler._stations;
+            })
+            .catch(function (err) {
+                console.log(err);
+            }).finally(() => {
+                console.log("finished loading stations")
+            });
+    }
+    loadRides() {
+        let dataHandler = this;
+        let USE_MIN = true;
+        console.log("loading bikes")
+        return Promise.all([...this.files.map(f => d3.csv("data/" + (USE_MIN ? "min/" : "") + f, d3.autoType))])
+            .then(function (data) {
                 // process ride data
                 dataHandler._rides = data.flat(1);
 
@@ -61,31 +82,30 @@ class DataHandler {
                 let dateParser2 = d3.timeParse("%Y-%m-%d %H:%M:%S"); // later csv have starttime with seconds with decimals. eg 42 sec vs. 42.48 seconds.
 
                 dataHandler._rides.forEach(d => {
-                    if(d.starttime){
+                    if (d.starttime) {
                         // add age attribute to data
-                        d.age = Number(d.starttime.slice(0,4)) - d["birth year"];
+                        d.age = Number(d.starttime.slice(0, 4)) - d["birth year"];
                         d.starttime = dateParser(d.starttime);
                     }
                     if (d.stoptime) {
                         d.stoptime = dateParser(d.stoptime);
                     }
                     let getYear = d3.timeParse("%Y");
-                   // d.age = getYear(d.starttime) //- d["birth year"];
+                    // d.age = getYear(d.starttime) //- d["birth year"];
                 });
 
-                console.log("data merge", dataHandler._stations, dataHandler._rides);
-                console.log("one station");
-                console.table(dataHandler._stations[0])
+                console.log("rides", dataHandler._rides);
                 console.log("one ride");
                 console.table(dataHandler._rides[0])
             })
             .catch(function (err) {
                 console.log(err);
+            }).finally(() => {
+                console.log("finished loading bikes")
             });
-
     }
     // group data by date
-    groupDate(){
+    groupDate() {
         let dataHandler = this;
         let groupedDate = {};
 
@@ -93,7 +113,7 @@ class DataHandler {
             let timeFormat = d3.timeFormat("%Y-%m-%d");
             let date = timeFormat(d.starttime);
 
-            if(groupedDate[date]){
+            if (groupedDate[date]) {
                 groupedDate[date].push(d);
             }
             else {
