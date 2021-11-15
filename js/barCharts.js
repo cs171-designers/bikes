12b55c0475d14efbc3a133bc88f9278fb93f8c53
Lara Zeng <40585319+larajz@users.noreply.github.com>
2021-11-15 01:47:38 -0500

    barchart visible but not completely working

--- js/barCharts.js
+++ js/barCharts.js
@@ -2,7 +2,7 @@
 
 class barChart {
     constructor(parentElement, ridesData, stationData){
-                // , variable) {
+        // , variable) {
         this.parentElement = parentElement;
         this.ridesData = ridesData;
         this.stationData = stationData;
@@ -15,13 +15,12 @@ class barChart {
     initVis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 10, bottom: 20, left: 50};
-
+        // margin conventions
+        vis.margin = { top: 10, right: 50, bottom: 10, left: 50 };
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.top - vis.margin.bottom;
 
-
-        // SVG drawing area
+        // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
             .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
@@ -30,7 +29,7 @@ class barChart {
 
 
         // Scales and axes
-        vis.x = d3.scaleTime()
+        vis.x = d3.scaleBand()
             .range([0, vis.width]);
 
         vis.y = d3.scaleLinear()
@@ -80,8 +79,10 @@ class barChart {
 
             let stationLatitude = vis.stationData[stationID][0]['Latitude']
             let stationLongitude = vis.stationData[stationID][0]['Longitude']
+            let name = vis.stationData[stationID][0]['Name']
             // console.log(stationLatitude)
-            sorted.push({id: stationID, numTrips: numTrips,
+            sorted.push({id: stationID, name: name,
+                numTrips: numTrips,
                 lat: stationLatitude, long: stationLongitude})
             // console.log(numTrips);
 
@@ -100,12 +101,58 @@ class barChart {
         vis.topFiveStations = vis.newsorted.slice(0,5)
         console.log(vis.topFiveStations)
 
+
+        vis.topFiveStations.forEach(function(station) {
+
+        })
+
         vis.updateVis();
 
     }
 
     updateVis() {
         let vis = this;
+
+        vis.x.domain(vis.topFiveStations.map(function(d) { return d.name; }))
+            .padding(0.2);
+
+        vis.svg.append("g")
+            .attr("transform", "translate(0," + vis.height + ")")
+            .call(vis.xAxis)
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+
+        vis.y.domain([0, d3.max(vis.topFiveStations, function (d) {
+            return d['numTrips']})]);
+
+        vis.svg.append("g")
+            .call(vis.yAxis)
+        console.log("y scale domain", vis.y.domain())
+        console.log("y scale range", vis.y.range())
+
+        console.log("x scale domain", vis.x.domain())
+
+        vis.svg.selectAll("mybar")
+            .data(vis.topFiveStations)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return vis.x(d.name); })
+            .attr("width", vis.x.bandwidth())
+            .attr("fill", "#69b3a2")
+            // no bar at the beginning thus:
+            .attr("height", function(d) { return (vis.height - vis.y(d.numTrips))})
+                //vis.height - vis.y(0); }) // always equal to 0
+            .attr("y", function(d) { return vis.y(0); })
+
+        // // Animation
+        // vis.svg.selectAll("rect")
+        //     .transition()
+        //     .duration(800)
+        //     .attr("y", function(d) { return vis.y(d.numTrips); })
+        //     .attr("height", function(d) { return -(vis.height - vis.y(d.numTrips)); })
+        //     .delay(function(d,i){console.log(i) ; return(i*100)})
+
 
 
     }
