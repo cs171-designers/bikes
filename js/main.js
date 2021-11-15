0653c702590c87b52080c28218721616ddb16b34
Plazma343 <46907783+Plazma343@users.noreply.github.com>
2021-11-15 00:32:22 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function init() {
 
         // map
         let bikeData = dataHandler.groupBikeID()
-        bikeMap = new BlueBikeMap("bike-map", bikeData, [42.360082, -71.058880])
+        bikeMap = new BlueBikeMap("bike-map", bikeData, dataHandler._stations, [42.360082, -71.058880])
 
         // pieChart
         let counts = dataHandler.getMultiLevelCounts();
