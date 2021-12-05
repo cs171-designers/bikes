9b78c6dc7ace80a1572ed8b3c55243ba861c79bf
Plazma343 <46907783+Plazma343@users.noreply.github.com>
2021-12-05 00:24:48 -0500

    Nightingale

--- js/nightingale.js
+++ js/nightingale.js
@@ -15,7 +15,7 @@ class NightingaleChart {
     initVis() {
         let vis = this;
 
-        vis.margin = { top: 20, right: 10, bottom: 20, left: 50 };
+        vis.margin = { top: 10, right: 10, bottom: 10, left: 10 };
 
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
@@ -36,10 +36,10 @@ class NightingaleChart {
 
         // Scales and labels
         vis.colorScale = d3.scaleLinear()
-            .range(["red", "blue"])
+            .range(["#deeff5", "#00008B"])
 
         vis.radiusScale = d3.scaleLinear()
-            .range([0, d3.min([vis.width/2, vis.height/2])])
+            .range([0, d3.min([vis.width/2, vis.height/2]) - vis.margin.top])
 
         vis.startAngleScale = d3.scaleLinear()
             .domain([0, 7]) // number of bins
@@ -50,14 +50,7 @@ class NightingaleChart {
             .range([Math.PI/4, 2*Math.PI])
 
         // format ticks to convey hour categories. Categories do not update
-        vis.tickStrings = ["12-3 am", "3-6 am", "6-9 am", "9-12 pm", "12-3 pm", "3-6 pm", "6-9 pm", "9-12 am"];
-
-        // add chart title placeholder
-        vis.svg.append("text")
-            .attr("x", vis.width / 2)
-            .attr("y", 0)
-            .attr("class", "lineTitle")
-            .text("Nightingale Chart");
+        vis.tickStrings = ["12am - 3am", "3am - 6am", "6am - 9am", "9am - 12pm", "12pm - 3pm", "3pm - 6pm", "6pm - 9pm", "9pm - 12am"];
 
         // (Filter, aggregate, modify data)
         vis.wrangleData();
@@ -119,7 +112,7 @@ class NightingaleChart {
             }
             return [num_rides, avg_trip_duration];
         }
-        console.log("BAR displayData", vis.displayData);
+        // console.log("BAR displayData", vis.displayData);
 
         // Create arrays of just num_rides and avg trip duration
         let numRidesArray = []
@@ -128,12 +121,11 @@ class NightingaleChart {
             numRidesArray.push(Math.sqrt(8*bin.num_rides/Math.PI));
             avgTripDurArray.push(bin.avg_trip_dur);
         })
-        console.log(numRidesArray)
-        console.log(avgTripDurArray)
 
         // Finish creating domains using wrangled data
-        vis.radiusScale.domain([0, d3.max(numRidesArray)])
-        vis.colorScale.domain([d3.min(avgTripDurArray), d3.max(avgTripDurArray)])
+        vis.maxRadVal = d3.max(numRidesArray)
+        vis.radiusScale.domain([0, vis.maxRadVal])
+        vis.colorScale.domain([0, avgTripDurArray.length])
 
 
         vis.updateVis();
@@ -143,10 +135,68 @@ class NightingaleChart {
     updateVis() {
         let vis = this;
 
+        let labelArcs = vis.nightingaleChartGroup.selectAll(".labelArc")
+            .data(vis.displayData)
+
+        labelArcs.enter().append("g")
+            .append("path")
+            .attr("class", "labelArc")
+            .attr("d", d3.arc()
+                .innerRadius(0)
+                .outerRadius(vis.radiusScale(vis.maxRadVal))
+                .startAngle(d => vis.startAngleScale(d.index))
+                .endAngle(d => vis.endAngleScale(d.index))
+            )
+            .attr("fill", "white")
+            .attr("stroke", "black")
+            .attr("stroke-width", "1px")
+            .each(function(d,i) {
+                // This section made with the help of this blog post by Nadleh Bremer
+                // https://www.visualcinnamon.com/2015/09/placing-text-on-arcs/
+                //A regular expression that captures all in between the start of a string
+                //(denoted by ^) and the first capital letter L
+                let firstArcSection = /(^.+?)L/;
+
+                //The [1] gives back the expression between the () (thus not the L as well)
+                //which is exactly the arc statement
+                let newArc = firstArcSection.exec(d3.select(this).attr("d") )[1];
+                //Replace all the comma's so that IE can handle it -_-
+                //The g after the / is a modifier that "find all matches rather than
+                //stopping after the first match"
+                newArc = newArc.replace(/,/g , " ");
+
+
+                //If the end angle lies beyond a quarter of a circle (90 degrees or pi/2)
+                //flip the end and start position
+                if (vis.endAngleScale(d.index) > 90 * Math.PI/180 && vis.endAngleScale(d.index) <= 3 * Math.PI/2) {
+                    //Everything between the capital M and first capital A
+                    let startLoc = /M(.*?)A/;
+                    //Everything between the capital A and 0 0 1
+                    let middleLoc = /A(.*?)0 0 1/;
+                    //Everything between the 0 0 1 and the end of the string (denoted by $)
+                    let endLoc = /0 0 1 (.*?)$/;
+                    //Flip the direction of the arc by switching the start and end point
+                    //and using a 0 (instead of 1) sweep flag
+                    let newStart = endLoc.exec( newArc )[1];
+                    let newEnd = startLoc.exec( newArc )[1];
+                    let middleSec = middleLoc.exec( newArc )[1];
+
+                    //Build up the new arc notation, set the sweep-flag to 0
+                    newArc = "M" + newStart + "A" + middleSec + "0 0 0 " + newEnd;
+                }
+
+                //Create a new invisible arc that the text can flow along
+                vis.nightingaleChartGroup.append("path")
+                    .attr("class", "hiddenDonutArcs")
+                    .attr("id", "timeArc_"+i)
+                    .attr("d", newArc)
+                    .style("fill", "none");
+            });
+
         let arcs = vis.nightingaleChartGroup.selectAll(".nightArc")
             .data(vis.displayData)
 
-        arcs.enter()
+        arcs.enter().append("g")
             .append("path")
             .attr("class", "nightArc")
             .attr("d", d3.arc()
@@ -161,17 +211,19 @@ class NightingaleChart {
             .attr("stroke", "black")
             .style("stroke-width", "1px")
 
+
         let arcLabels = vis.nightingaleChartGroup.selectAll(".arcLabel")
             .data(vis.displayData)
 
-        arcLabels.enter().append("g")
-            .append("text")
+        arcLabels.enter().append("text")
             .attr("class", "arcLabel")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("text-anchor", "middle")
-            .attr("x", 0)
-            .attr("y", 0)
+            .attr("dy", function(d,i) {
+                return (vis.endAngleScale(d.index) > 90 * Math.PI/180 && vis.endAngleScale(d.index) <= 3 * Math.PI/2 ? 18 : -11);
+            })
+            .append("textPath")
+            .attr("startOffset","50%")
+            .style("text-anchor","middle")
+            .attr("xlink:href", function(d,i) {return "#timeArc_"+i;})
             .text(d => vis.tickStrings[d.index])
 
     }
