77b8da96f51ac26f109689f4eb2d4cc45acc82ee
wue22 <54972594+wue22@users.noreply.github.com>
2021-12-05 02:47:47 -0500

    margin change for bar chart

--- js/stationBarCharts.js
+++ js/stationBarCharts.js
@@ -17,7 +17,7 @@ class StationBarChart {
         let vis = this;
 
         // margin conventions
-        vis.margin = { top: 30, right: 50, bottom: 250, left: 180 }; // left margin 180
+        vis.margin = { top: 30, right: 50, bottom: 250, left: 80 }; // left margin 180
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.top - vis.margin.bottom;
 
@@ -64,7 +64,7 @@ class StationBarChart {
 
         // add chart title placeholder
         vis.svg.append("text")
-            .attr("x",-60 + vis.width/2)//vis.margin.left + vis.width/2)
+            .attr("x",-vis.margin.left + vis.width/2)//vis.margin.left + vis.width/2)
             .attr("y",-15)
             .attr("class","lineTitle");
 
