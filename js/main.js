6be7fb04e64774905cf1d3b5a1d4580096f22f1b
Lara Zeng <40585319+larajz@users.noreply.github.com>
2021-12-05 02:12:15 -0500

    indexxxxxxx

--- js/main.js
+++ js/main.js
@@ -223,3 +223,15 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("orientationchange", lazyLoad);
     lazyLoad();
 });
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
